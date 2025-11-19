4c2f6a8d848cb6b5b09152a00d040032ac60bdbd
dreamshadow1216-gif <dreamshadow1216@gmail.com>
2025-11-18 18:59:49 -0600

    Update television.py

--- television.py
+++ television.py
@@ -5,19 +5,23 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self) -> None:
+      """ setting default values """
         self.__status = False
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
 
     def power(self) -> None:
+      """ turning on tv """
         self.__status = not self.__status
 
     def mute(self) -> None:
+      """ muting tv """
         if self.__status:
             self.__muted = not self.__muted
 
     def channel_up(self) -> None:
+      """ turning the channel up """
         if self.__status:
             if self.__channel == Television.MAX_CHANNEL:
                 self.__channel = Television.MIN_CHANNEL
@@ -25,6 +29,7 @@ class Television:
                 self.__channel += 1
 
     def channel_down(self) -> None:
+      """ turning the channel down """
         if self.__status:
             if self.__channel == Television.MIN_CHANNEL:
                 self.__channel = Television.MAX_CHANNEL
@@ -32,6 +37,7 @@ class Television:
                 self.__channel -= 1
 
     def volume_up(self) -> None:
+      """ turning the volume up """
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -39,6 +45,7 @@ class Television:
                 self.__volume += 1
 
     def volume_down(self) -> None:
+      """ turning the volume down """ 
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -46,4 +53,5 @@ class Television:
                 self.__volume -= 1
 
     def __str__(self) -> str:
+      """ displaying tv values :return: power, channel, and volume changes """
         return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
